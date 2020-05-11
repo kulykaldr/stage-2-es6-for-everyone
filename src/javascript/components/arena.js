cbb1e6dbfe2f9c7e34ddb06324645a24891a4f7d
Kulyk Aleksandr <16745608+kulykaldr@users.noreply.github.com>
2020-05-11 17:30:29 +0300

    Add fight feature

--- src/javascript/components/arena.js
+++ src/javascript/components/arena.js
@@ -1,7 +1,9 @@
 import { createElement } from '../helpers/domHelper';
 import { createFighterImage } from './fighterPreview';
+import { fight } from './fight';
+import { showWinnerModal } from './modal/winner';
 
-export function renderArena(selectedFighters) {
+export async function renderArena(selectedFighters) {
   const root = document.getElementById('root');
   const arena = createArena(selectedFighters);
 
@@ -11,13 +13,16 @@ export function renderArena(selectedFighters) {
   // todo:
   // - start the fight
   // - when fight is finished show winner
+  const winner = await fight(...selectedFighters);
+  showWinnerModal(winner);
+  // fight(...selectedFighters).then((winner) => showWinnerModal(winner));
 }
 
 function createArena(selectedFighters) {
   const arena = createElement({ tagName: 'div', className: 'arena___root' });
   const healthIndicators = createHealthIndicators(...selectedFighters);
   const fighters = createFighters(...selectedFighters);
-  
+
   arena.append(healthIndicators, fighters);
   return arena;
 }
@@ -37,7 +42,11 @@ function createHealthIndicator(fighter, position) {
   const container = createElement({ tagName: 'div', className: 'arena___fighter-indicator' });
   const fighterName = createElement({ tagName: 'span', className: 'arena___fighter-name' });
   const indicator = createElement({ tagName: 'div', className: 'arena___health-indicator' });
-  const bar = createElement({ tagName: 'div', className: 'arena___health-bar', attributes: { id: `${position}-fighter-indicator` }});
+  const bar = createElement({
+    tagName: 'div',
+    className: 'arena___health-bar',
+    attributes: { id: `${position}-fighter-indicator` },
+  });
 
   fighterName.innerText = name;
   indicator.append(bar);
--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,3 +1,8 @@
+import { showModal } from './modal';
+import { createFighterImage } from '../fighterPreview';
+import { fight } from '../fight';
+
 export function showWinnerModal(fighter) {
-  // call showModal function 
+  // call showModal function
+  showModal({ title: 'WINNER', bodyElement: fighter.name });
 }
